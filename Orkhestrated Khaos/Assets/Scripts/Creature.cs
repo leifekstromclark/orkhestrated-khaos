7d109f9748fd910096ad2cf5d6ce11da3b61f2e6
leifekstromclark <59668580+leifekstromclark@users.noreply.github.com>
2021-07-10 20:55:33 -0600

    Added a test background

---
+++ Orkhestrated Khaos/Assets/Scripts/Creature.cs
@@ -0,0 +1,15 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+public class Creature
+{
+    public int power;
+    public int health;
+    public int speed;
+    public int range;
+    public int attacks;
+    public int cost;
+    public int upkeep;
+    public int loyalty;
+}
