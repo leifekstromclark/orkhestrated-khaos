0aff2226de573228bd0e0ca176cc6b77f9ece807
leifekstromclark <59668580+leifekstromclark@users.noreply.github.com>
2021-07-14 00:59:44 -0600

    messed around with an ork and his animation

--- Orkhestrated Khaos/Assets/Scripts/Unit.cs
+++ Orkhestrated Khaos/Assets/Scripts/Unit.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+
+    public int power;
+    public int health;
+    public int speed;
+    public int range;
+    public int attacks;
+    public int cost;
+    public int upkeep;
+    public int loyalty;
+
     // Start is called before the first frame update
     void Start()
     {
